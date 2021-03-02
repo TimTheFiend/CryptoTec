37750658fcc1fa8d1fba1ffbb6e3353a12f15066
TimTheFiend <33222649+TimTheFiend@users.noreply.github.com>
2021-03-02 08:37:23 +0100

    Initial changes

--- Program.cs
+++ Program.cs
@@ -17,8 +17,30 @@ namespace CryptoTec
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                #region `ConfigureServices has been added.`
+                .ConfigureServices((context, services) => {
+                    HostConfig.CertPath = context.Configuration["CertPath"];
+                    HostConfig.CertPassword = context.Configuration["CertPassword"];
+                })
+            #endregion
                 .ConfigureWebHostDefaults(webBuilder => {
+                    #region This has been changed.
+                    var host = System.Net.Dns.GetHostEntry("cryptotec.dk");
+                    webBuilder.ConfigureKestrel(opt => {
+                        opt.Listen(host.AddressList[0], 80);
+                        opt.Listen(host.AddressList[0], 443, listOpt => {
+                            listOpt.UseHttps(HostConfig.CertPath, HostConfig.CertPassword);
+                        });
+                    });
+                    #endregion
                     webBuilder.UseStartup<Startup>();
                 });
     }
+
+    public static class HostConfig
+    {
+        public static string CertPath { get; set; }
+        public static string CertPassword { get; set; }
+    }
+
 }
