6a77c8fc9bf0bbfb8d90699e255cd5921327829f
TimTheFiend <33222649+TimTheFiend@users.noreply.github.com>
2021-03-04 11:51:39 +0100

    init

--- Startup.cs
+++ Startup.cs
@@ -60,7 +60,12 @@ namespace CryptoTec
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+                endpoints.MapControllerRoute(
+                    name: "user",
+                    pattern: "{controller=User}/{action=TodoList}/{id?}"
+                );
             });
         }
     }
